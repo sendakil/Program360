82d36745cc57d79d88592c94f871afcd490718fa
Rajagopal Senthil Kumar <sendakil@yahoo.com>
2017-09-04 16:47:38 +0800

    Modified PDF Reader

--- PyV2/Py/Py.py
+++ PyV2/Py/Py.py
@@ -1,9 +1,28 @@
 
 from etl.pdf.pdf_reader import PdfReader 
 
-pdf01=PdfReader("c:\\Me\\Python\\MySample01.pdf",1)
+config = dict(DRIVER='{ODBC Driver 11 for SQL Server}',
+                server=   'RAJASENTHIL\SQLEXPRESS',
+              port=      1433,                   
+              database= 'Taxi',
+              username= 'sa',
+              password= 'shambhavvi')
+
+input_pdf_file="C:\\Program360\\Sample\\MySample01.pdf"
+output_file="C:\\Program360\\Sample\\MySample01.csv"
+output_json_file="C:\\Program360\\Sample\\MySample01.json"
+conn_str='DRIVER={SQL Server Native Client 11.0};SERVER='+config['server']+ ';DATABASE='+config['database']+';UID='+config['username']+';PWD='+ config['password']
+
+pdf01=PdfReader(input_pdf_file,1)
 print(pdf01.getAuthor())
 print(pdf01.getVersion())
-pdf01.countLayoutObjects(1)
-print(pdf01.extracted_texts)
-print(pdf01.layout_objects_count)
+#pdf01.countLayoutObjects(1)
+#print(pdf01.extracted_texts)
+#print(pdf01.layout_objects_count)
+
+#pdf01.saveTextFile(output_file)
+#pdf01.readTextFile(output_file)
+#pdf01.saveMsSql(conn_str)
+#pdf01.readMsSql(conn_str)
+pdf01.saveJSON(output_json_file)
+pdf01.readJSON(output_json_file)
--- PyV2/Py/etl/pdf/pdf_reader.py
+++ PyV2/Py/etl/pdf/pdf_reader.py
@@ -17,7 +17,8 @@ from pdfminer.pdfinterp import PDFResourceManager,PDFPageInterpreter
 from pdfminer.pdfdevice import PDFDevice
 from pdfminer.layout import LAParams,LTTextBoxHorizontal,LTImage,LTFigure,LTRect,LTTextBox,LTLine
 import io
-
+import pyodbc
+import json
 
 
 class PdfReader(object):
@@ -30,15 +31,14 @@ class PdfReader(object):
 
     def __init__(self,file_name="",extract_yes=0):
        PdfReader.no_of_objects+=1 
-       self.pdf_resource=PDFResourceManager()
+       self._pdf_resource=PDFResourceManager()
        self._pdf_params=LAParams()
-       self.pdf_device=PDFPageAggregator(self.pdf_resource,laparams=self._pdf_params)
-       self.pdf_interpreter=PDFPageInterpreter(self.pdf_resource,self.pdf_device)
+       self._pdf_device=PDFPageAggregator(self._pdf_resource,laparams=self._pdf_params)
+       self._pdf_interpreter=PDFPageInterpreter(self._pdf_resource,self._pdf_device)
        self.layout_objects_count={'no_of_images':0,'no_of_figures':0,'no_of_rect':0,'no_of_text':0,'no_of_othertext':0}
        self.extracted_texts=[]
        if file_name!="":
-           self.file_name=file_name
-           self.pdf_file =open(file_name,'rb')
+           self.loadPDFdocument(file_name)
            if extract_yes!=0:
                self.countLayoutObjects(extract_yes)
 
@@ -50,15 +50,19 @@ class PdfReader(object):
       
      
     def loadPDFdocument(self,file_name):
-        self.file_name=file_name
-        self.pdf_file =open(file_name,'rb')
-            
+        try:
+            self.file_name=file_name
+            self.pdf_document =open(file_name,'rb')
+        except IOError:
+            print("Error: Can\'t file or read data")
+        
+               
       
     def countLayoutObjects(self,extract_yes=0):
         
-        for pdf_page in PDFPage.get_pages(self.pdf_file):
-            self.pdf_interpreter.process_page(pdf_page)
-            pdf_layout=self.pdf_device.get_result()
+        for pdf_page in PDFPage.get_pages(self.pdf_document):
+            self._pdf_interpreter.process_page(pdf_page)
+            pdf_layout=self._pdf_device.get_result()
             for layout_element in pdf_layout:
                 if isinstance(layout_element,LTFigure):
                     self.layout_objects_count['no_of_figures']+=1
@@ -75,4 +79,97 @@ class PdfReader(object):
                 else:
                     self.layout_objects_count['no_of_othertext']+=1
 
+    def saveTextFile(self,file_name,col_delimiter=","):
+        try:
+            txt_file=open(file_name,"w+")
+            txt_file.write("pdf_file,Seq,Content\n")
+            i=1
+            for extracted_text in self.extracted_texts:
+                txt_cleaned=(extracted_text.strip()).replace("\n","|")
+                txt_line=self.file_name+","+str(i)+col_delimiter+txt_cleaned+'\n'
+                txt_file.write(txt_line)
+                i+=1
+        except IOError:
+              print("Error: Can\'t file or read data")
 
+
+    def readTextFile(self,file_name):
+        try:
+            txt_file=open(file_name,"r+")
+            file_content=txt_file.readlines()
+            for file_line in file_content:
+                print(file_line)
+                
+        except IOError:
+              print("Error: Can\'t file or read data")
+
+
+
+    def saveMsSql(self,conn_str):
+        try:
+         conn =pyodbc.connect(conn_str)
+         record_set=conn.cursor()
+         insert_sql_str= "INSERT INTO dbo.pdf_extract(file_name, seq, content) values (?,?,?)"
+         i=1
+         for extracted_text in self.extracted_texts:
+                txt_cleaned=(extracted_text.strip()).replace("\n","|")
+                record_set.execute(insert_sql_str,self.file_name,i,txt_cleaned)
+                i+=1
+         record_set.commit()
+         record_set.close()
+
+        except Exception:
+             print("Error: MS Sql connection ")
+
+
+
+    def readMsSql(self,conn_str):
+        try:
+         conn =pyodbc.connect(conn_str)
+         record_set=conn.cursor()
+         select_sql_str= "select file_name, seq, content from dbo.pdf_extract"
+         record_set.execute(select_sql_str)
+         
+         for row in record_set.fetchall():
+             print(row)
+
+        except Exception:
+             print("Error: MS Sql connection ")
+
+
+
+    def saveJSON(self,file_name):
+        try:
+            json_dataset={}
+            json_dataset['pdf_recordset']=[]
+         
+            i=1
+            for extracted_text in self.extracted_texts:
+                txt_cleaned=(extracted_text.strip()).replace("\n","|")
+                tx_json_format={'file_name':self.file_name,'i':i,'Content':txt_cleaned}
+                json_dataset['pdf_recordset'].append(tx_json_format)
+                i+=1
+                
+            json_file=open(file_name,"w+")
+            json.dump(json_dataset,json_file)
+
+        except IOError:
+              print("Error: Can\'t file or read data")
+
+
+    def readJSON(self,file_name):
+        try:
+ 
+                
+            json_file=open(file_name,"r+")
+            json_dataset=json.load(json_file)
+
+            for json_record in json_dataset['pdf_recordset']:
+                print('File Name:' ,json_record['file_name'])
+                print('Content:', json_record['Content'])
+                
+        except IOError:
+              print("Error: Can\'t file or read data")
+
+
+   
